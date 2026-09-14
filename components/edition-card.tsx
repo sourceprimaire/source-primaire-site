cd90a5e4c4afb3eb336eabde90d89cdfa7578e8e
Source Primaire <sourceprimaire.fr@gmail.com>
2026-09-14 11:39:20 +0200

    Separate edition numbers and titles with a dash

--- components/edition-card.tsx
+++ components/edition-card.tsx
@@ -11,7 +11,7 @@ export function EditionCard({ edition, locale = 'fr' }: { edition: Edition; loca
         <img src={edition.cover} alt={edition.title} />
       </figure>
       <div className="edition-caption">
-        <span>{edition.index}&nbsp;{edition.title}</span>
+        <span>{edition.index}-{edition.title}</span>
         <span>{edition.type}</span>
       </div>
     </a>
